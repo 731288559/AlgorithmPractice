485bf2d63880d197242f3a9bffdd800fcc0bc004
chenjiayu <731288559@qq.com>
2023-09-19 00:32:48 +0800

    update: 添加文件

---
+++ leetcode/LC213_test.go
@@ -0,0 +1,43 @@
+package leetcode
+
+import "testing"
+
+func Test_rob2(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "",
+			args: args{
+				nums: []int{2, 3, 2},
+			},
+			want: 3,
+		},
+		{
+			name: "",
+			args: args{
+				nums: []int{1, 2, 3, 1},
+			},
+			want: 4,
+		},
+		{
+			name: "",
+			args: args{
+				nums: []int{1, 2, 3},
+			},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rob2(tt.args.nums); got != tt.want {
+				t.Errorf("rob2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
