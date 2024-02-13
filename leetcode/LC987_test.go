3f52152a88d319a77b488f6611bbe77b69b62d81
jerryychen <jerryychen@futunn.com>
2024-02-13 11:11:05 +0800

    update: 987

---
+++ leetcode/LC987_test.go
@@ -0,0 +1,40 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_verticalTraversal(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "",
+			args: args{
+				root: &TreeNode{
+					Val:  3,
+					Left: &TreeNode{Val: 9},
+					Right: &TreeNode{
+						Val:   20,
+						Left:  &TreeNode{Val: 15},
+						Right: &TreeNode{Val: 7},
+					},
+				},
+			},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := verticalTraversal(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("verticalTraversal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
